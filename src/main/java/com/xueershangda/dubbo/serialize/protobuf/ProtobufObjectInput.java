dab2367f07b3f6fc32f0320cbb62b6f8bdbd3a6d
skmbw <tongku2008@126.com>
2018-09-11 17:41:51 +0800

    存在直接调用readObject()的情况，比如无参的方法，异常等会使用。所以还是需要保存类型信息的。无参的是不是不需要处理了啊

--- src/main/java/com/xueershangda/dubbo/serialize/protobuf/ProtobufObjectInput.java
+++ src/main/java/com/xueershangda/dubbo/serialize/protobuf/ProtobufObjectInput.java
@@ -1,13 +1,21 @@
 package com.xueershangda.dubbo.serialize.protobuf;
 
-import com.alibaba.dubbo.common.URL;
+import com.alibaba.dubbo.common.compiler.support.ClassUtils;
 import com.alibaba.dubbo.common.serialize.ObjectInput;
-import io.protostuff.ByteArrayInput;
+import io.protostuff.MessageCollectionSchema;
+import io.protostuff.ProtostuffIOUtil;
+import io.protostuff.Schema;
+import io.protostuff.StringMapSchema;
+import io.protostuff.runtime.RuntimeSchema;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Type;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.util.*;
 
 /**
  * @author yinlei
@@ -15,60 +23,215 @@ import java.lang.reflect.Type;
  */
 public class ProtobufObjectInput implements ObjectInput {
 
-    private ByteArrayInput input;
-    private byte[] buffer;
+    private byte[] bytes;
+    private ByteBuffer byteBuffer;
 
-    public ProtobufObjectInput(URL url, InputStream stream) throws IOException {
-        byte[] buffer = IOUtils.toByteArray(stream);
-        this.buffer = buffer;
-        input = new ByteArrayInput(buffer, 0, 0, false);
+    public ProtobufObjectInput(InputStream inputStream) throws IOException {
+        bytes = IOUtils.toByteArray(inputStream);
+        byteBuffer = ByteBuffer.wrap(bytes);
     }
 
-    public Object readObject() throws IOException, ClassNotFoundException {
+    @Override
+    public boolean readBool() throws IOException {
+        byteBuffer.get();
+        return byteBuffer.get() != 0;
+    }
+
+    @Override
+    public byte readByte() throws IOException {
+        byteBuffer.get();
+        return byteBuffer.get();
+    }
+
+    @Override
+    public short readShort() throws IOException {
+        byteBuffer.get();
+        return byteBuffer.getShort();
+    }
+
+    @Override
+    public int readInt() throws IOException {
+        byteBuffer.get();
+        return byteBuffer.getInt();
+    }
+
+    @Override
+    public long readLong() throws IOException {
+        byteBuffer.get();
+        return byteBuffer.getInt();
+    }
+
+    @Override
+    public float readFloat() throws IOException {
+        byteBuffer.get();
+        return byteBuffer.getFloat();
+    }
+
+    @Override
+    public double readDouble() throws IOException {
+        byteBuffer.get();
+        return byteBuffer.getDouble();
+    }
+
+    @Override
+    public String readUTF() throws IOException {
+        byte type = byteBuffer.get();
+        if (type != 12) { // 不是string类型
+            return null;
+        }
+        return readString();
+    }
+
+    private String readString() throws IOException {
+        int length = byteBuffer.getInt();
+        if (length != 0) {
+            byte[] data = new byte[length];
+            byteBuffer.get(data);
+            return new String(data, "UTF-8");
+        }
         return null;
     }
 
-    public <T> T readObject(Class<T> cls) throws IOException, ClassNotFoundException {
-        return ProtoUtils.fromByteArray(buffer, cls);
-    }
-
-    public <T> T readObject(Class<T> cls, Type type) throws IOException, ClassNotFoundException {
-        return ProtoUtils.fromByteArray(buffer, type);
-    }
-
-    public boolean readBool() throws IOException {
-        return input.readBool();
-    }
-
-    public byte readByte() throws IOException {
-        return (byte) input.readInt32();
-    }
-
-    public short readShort() throws IOException {
-        return (short) input.readInt32();
-    }
-
-    public int readInt() throws IOException {
-        return input.readInt32();
-    }
-
-    public long readLong() throws IOException {
-        return input.readInt64();
-    }
-
-    public float readFloat() throws IOException {
-        return input.readFloat();
-    }
-
-    public double readDouble() throws IOException {
-        return input.readDouble();
-    }
-
-    public String readUTF() throws IOException {
-        return input.readString();
-    }
-
+    @Override
     public byte[] readBytes() throws IOException {
-        return input.readByteArray();
+        byte type = byteBuffer.get();
+        if (type != 14) { // 不是byte[]
+            return new byte[0];
+        }
+        return getBytes();
+    }
+
+    private byte[] getBytes() throws IOException {
+        int length = byteBuffer.getInt();
+        if (length != 0) {
+            byte[] data = new byte[length];
+            byteBuffer.get(data);
+            return data;
+        }
+        return new byte[0];
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Object readObject() throws IOException, ClassNotFoundException {
+        if (bytes == null || bytes.length == 0) {
+            return null;
+        }
+
+        byte type = byteBuffer.get();
+        // 基本类型和复合类型在一起，导致获取数据长度有问题
+        switch (type) {
+            case 4:
+                return byteBuffer.getInt();
+            case 5:
+                return byteBuffer.getLong();
+            case 6:
+                return byteBuffer.getDouble();
+            case 7:
+                String s = readString();
+                return new BigInteger(s == null ? "0" : s);
+            case 8:
+                s = readString();
+                return new BigDecimal(s == null ? "0" : s);
+            case 9:
+                // 标志位 不想 再重置了
+                return byteBuffer.get();
+            case 10:
+                return byteBuffer.getFloat();
+            case 11:
+                return byteBuffer.getShort();
+            case 12:
+                return readString();
+            case 13:
+                return byteBuffer.get() != 0;
+            case 14:
+                return getBytes();
+//            case 15: // 数组的序列化是支持的，但是性能没有List好，建议使用List代替
+//                throw new UnsupportedEncodingException("Please use List instead of.");
+            case 16: // 异常
+                int totalLength = byteBuffer.getInt();
+                int nameLength = byteBuffer.getInt();
+                byte[] classNameBytes = new byte[nameLength];
+                byteBuffer.get(classNameBytes);
+                String message = "";
+                if (totalLength > nameLength) {
+                    int messageLength = totalLength - nameLength;
+                    byte[] messageBytes = new byte[messageLength];
+                    byteBuffer.get(messageBytes);
+                    message = new String(messageBytes, "UTF-8");
+                }
+                String className = new String(classNameBytes, "UTF-8");
+                return new RuntimeException(className + ";message=" + message);
+        }
+
+        // 集合和对象类型和基本类型分开，代码更整洁
+        int totalLength = byteBuffer.getInt();
+        if (totalLength == 0) {
+            switch (type) {
+                case 0: // 对象
+                    return null;
+                case 1:
+                    return Collections.emptyList();
+                case 2:
+                    return Collections.emptySet();
+                case 3:
+                    return Collections.emptyMap();
+            }
+        }
+        int nameLength = byteBuffer.getInt();
+        byte[] nameBytes = new byte[nameLength];
+        byteBuffer.get(nameBytes);
+        String className = new String(nameBytes, "UTF-8");
+        Class clazz = ClassUtils.forName(className);
+        Schema schema = RuntimeSchema.getSchema(clazz);
+
+        byte[] dataBytes = new byte[totalLength - nameLength - 9];
+        byteBuffer.get(dataBytes);
+        switch (type) {
+            case 0:
+                Object object = newInstance(clazz);
+                ProtostuffIOUtil.mergeFrom(dataBytes, object, schema);
+                return object;
+            case 1:
+                MessageCollectionSchema collectionSchema = new MessageCollectionSchema(schema);
+                List list = new ArrayList();
+                ProtostuffIOUtil.mergeFrom(dataBytes, list, collectionSchema);
+                return list;
+            case 2:
+                collectionSchema = new MessageCollectionSchema(schema);
+                Set set = new HashSet();
+                ProtostuffIOUtil.mergeFrom(dataBytes, set, collectionSchema);
+                return set;
+            case 3:
+                StringMapSchema stringSchema = new StringMapSchema(schema);
+                Map map = new HashMap();
+                ProtostuffIOUtil.mergeFrom(dataBytes, map, stringSchema);
+                return map;
+            default:
+
+        }
+        return null;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T readObject(Class<T> cls) throws IOException, ClassNotFoundException {
+        return (T) readObject();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T readObject(Class<T> cls, Type type) throws IOException, ClassNotFoundException {
+        return (T) readObject();
+    }
+
+    private Object newInstance(Class clazz) {
+        Object entity;
+        try {
+            entity = clazz.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException("实例化对象的异常", e);
+        }
+        return entity;
     }
 }
--- src/main/java/com/xueershangda/dubbo/serialize/protobuf/ProtobufObjectOutput.java
+++ src/main/java/com/xueershangda/dubbo/serialize/protobuf/ProtobufObjectOutput.java
@@ -1,12 +1,19 @@
 package com.xueershangda.dubbo.serialize.protobuf;
 
-import com.alibaba.dubbo.common.URL;
 import com.alibaba.dubbo.common.serialize.ObjectOutput;
-import io.protostuff.LinkedBuffer;
-import io.protostuff.ProtobufOutput;
+import io.protostuff.*;
+import io.protostuff.runtime.RuntimeSchema;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author yinlei
@@ -14,62 +21,309 @@ import java.io.OutputStream;
  */
 public class ProtobufObjectOutput implements ObjectOutput {
 
-    private ProtobufOutput output;
-    private OutputStream stream;
+    private OutputStream output;
+    private ByteBuffer byteBuffer;
 
-    public ProtobufObjectOutput(URL url, OutputStream stream) {
-        this.stream = stream;
-        this.output = new ProtobufOutput(LinkedBuffer.allocate());
+    public ProtobufObjectOutput(OutputStream output) {
+        this.output = output;
+        this.byteBuffer = ByteBuffer.allocate(1024);
     }
 
-    public void writeObject(Object obj) throws IOException {
-        byte[] byteArray = ProtoUtils.toByteArray(obj);
-        output.writeByteArray(1, byteArray, false);
-    }
-
+    @Override
     public void writeBool(boolean v) throws IOException {
-        output.writeBool(1, v, false);
+        check(2);
+        byteBuffer.put((byte) 13); // 数据类型
+        byteBuffer.put((byte) 1);
     }
 
+    @Override
     public void writeByte(byte v) throws IOException {
-        output.writeInt32(1, v, false);
+        check(2);
+        byteBuffer.put((byte) 9);
+        byteBuffer.put(v);
     }
 
+    @Override
     public void writeShort(short v) throws IOException {
-        output.writeInt32(1, v, false);
+        check(3);
+        byteBuffer.put((byte) 11);
+        byteBuffer.putShort(v);
     }
 
+    @Override
     public void writeInt(int v) throws IOException {
-        output.writeInt32(1, v, false);
+        check(5);
+        byteBuffer.put((byte) 4);
+        byteBuffer.putInt(v);
     }
 
+    @Override
     public void writeLong(long v) throws IOException {
-        output.writeInt64(1, v, false);
+        check(9);
+        byteBuffer.put((byte) 5);
+        byteBuffer.putLong(v);
     }
 
+    @Override
     public void writeFloat(float v) throws IOException {
-        output.writeFloat(1, v, false);
+        check(5);
+        byteBuffer.put((byte) 10);
+        byteBuffer.putFloat(v);
     }
 
+    @Override
     public void writeDouble(double v) throws IOException {
-        output.writeDouble(1, v, false);
+        check(9);
+        byteBuffer.put((byte) 6);
+        byteBuffer.putDouble(v);
     }
 
+    @Override
     public void writeUTF(String v) throws IOException {
-        output.writeString(1, v, false);
+        check(1);
+        byteBuffer.put((byte) 12); // 数据类型
+        if (v == null) {
+            check(4);
+            byteBuffer.putInt(0); // 长度为0
+        } else {
+            byte[] bytes = v.getBytes("UTF-8");
+            int len = bytes.length;
+            check(4 + len);
+            byteBuffer.putInt(len); // int 占 4 位
+            byteBuffer.put(bytes); // 存入数据
+        }
     }
 
+    private void writeBig(String v, byte type) throws IOException {
+        byte[] bytes = v.getBytes("UTF-8");
+        int len = bytes.length;
+        check(5 + len);
+        byteBuffer.put(type);
+        byteBuffer.putInt(len);
+        byteBuffer.put(bytes);
+    }
+
+    @Override
     public void writeBytes(byte[] v) throws IOException {
-        output.writeByteArray(1, v, false);
+        if (v == null || v.length == 0) {
+            check(5);
+            byteBuffer.put((byte) 14);
+            byteBuffer.putInt(0);
+        } else {
+            writeBytes(v, 0 , v.length);
+        }
     }
 
+    @Override
     public void writeBytes(byte[] v, int off, int len) throws IOException {
-        output.writeByteRange(true, 1, v, off, len, false);
+        check(1);
+        byteBuffer.put((byte) 14);
+        if (v == null) {
+            check(4);
+            byteBuffer.putInt(0);
+        } else {
+            check(4 + len);
+            byteBuffer.putInt(len);
+            byteBuffer.put(v, off, len);
+        }
     }
 
+    @Override
     public void flushBuffer() throws IOException {
-        byte[] byteArray = output.toByteArray();
-        stream.write(byteArray);
-        stream.flush();
+        byteBuffer.flip();
+        int limit = byteBuffer.limit();
+        byte[] bytes = new byte[limit];
+        byteBuffer.get(bytes);
+        output.write(bytes);
+        output.flush();
+    }
+
+    @Override
+    public void writeObject(Object obj) throws IOException {
+        if (obj == null) {
+            writeBool(true);
+            return;
+        }
+
+        Class cls;
+        if (obj instanceof List) {
+            List list = (List) obj;
+            if (list.isEmpty()) {
+                check(5);
+                byteBuffer.put((byte) 1); // 类型
+                byteBuffer.putInt(0); // 长度
+                return;
+            }
+            cls = list.get(0).getClass();
+            byte[] nameBytes = cls.getName().getBytes("UTF-8");
+            int nameLength = nameBytes.length;
+            byte[] listBytes = collectToBytes(cls, list);
+            int dataLength = listBytes.length;
+            // 9 = 1 + 4 + 4
+            int totalLength = 9 + nameLength + dataLength;
+            check(totalLength);
+            byteBuffer.put((byte) 1); // 类型
+            byteBuffer.putInt(totalLength);
+            byteBuffer.putInt(nameLength);
+            byteBuffer.put(nameBytes);
+            byteBuffer.put(listBytes);
+        } else if (obj instanceof Set) {
+            Set set = (Set) obj;
+            if (set.isEmpty()) {
+                check(5);
+                byteBuffer.put((byte) 2); // 类型
+                byteBuffer.putInt(0); // 长度
+                return;
+            }
+            cls = set.iterator().next().getClass();
+            byte[] nameBytes = cls.getName().getBytes("UTF-8");
+            int nameLength = nameBytes.length;
+            byte[] dataBytes = collectToBytes(cls, set);
+            int dataLength = dataBytes.length;
+            // 9 = 1 + 4 + 4
+            int totalLength = 9 + nameLength + dataLength;
+            check(totalLength);
+            byteBuffer.put((byte) 2); // 类型
+            byteBuffer.putInt(totalLength);
+            byteBuffer.putInt(nameLength);
+            byteBuffer.put(nameBytes);
+            byteBuffer.put(dataBytes);
+        } else if (obj instanceof Map) {
+            Map map = (Map) obj;
+            if (map.isEmpty()) {
+                check(5);
+                byteBuffer.put((byte) 3); // 类型
+                byteBuffer.putInt(0); // 长度
+                return;
+            }
+            // value对象的类型
+            cls = map.values().iterator().next().getClass();
+            byte[] nameBytes = cls.getName().getBytes("UTF-8");
+            int nameLength = nameBytes.length;
+            byte[] mapBytes = mapToBytes(cls, map);
+            int dataLength = mapBytes.length;
+            // 9 = 1 + 4 + 4
+            int totalLength = 9 + nameLength + dataLength;
+            check(totalLength);
+            byteBuffer.put((byte) 3); // 类型
+            byteBuffer.putInt(totalLength);
+            byteBuffer.putInt(nameLength);
+            byteBuffer.put(nameBytes);
+            byteBuffer.put(mapBytes);
+        } else if (obj instanceof Number) {
+            if (obj instanceof Integer) {
+                int v = (int) obj;
+                writeInt(v);
+            } else if (obj instanceof Long) {
+                long l = (long) obj;
+                writeLong(l);
+            } else if (obj instanceof Double) {
+                double d = (double) obj;
+                writeDouble(d);
+            } else if (obj instanceof BigInteger) {
+                BigInteger s = (BigInteger) obj;
+                String v = s.toString();
+                writeBig(v, (byte) 7);
+            } else if (obj instanceof BigDecimal) {
+                BigDecimal s = (BigDecimal) obj;
+                String v = s.toString();
+                writeBig(v, (byte) 8);
+            } else if (obj instanceof Byte) {
+                byte v = (byte) obj;
+                writeByte(v);
+            } else if (obj instanceof Float) {
+                float v = (float) obj;
+                writeFloat(v);
+            } else if (obj instanceof Short) {
+                short v = (short) obj;
+                writeFloat(v);
+            } else {
+                throw new RuntimeException("不支持的数字类型:" + obj.getClass().getName());
+            }
+        } else if (obj instanceof String) {
+            String v = (String) obj;
+            writeUTF(v);
+        } else if (obj instanceof Boolean) {
+            boolean b = (boolean) obj;
+            writeBool(b);
+        } else if (obj instanceof byte[]) {
+            byte[] bytes = (byte[]) obj;
+            writeBytes(bytes);
+        } else if (obj.getClass().isArray()) {
+            // 数组的序列化是支持的，但是性能没有List好，建议使用List代替
+            throw new UnsupportedEncodingException("Please use List instead of.");
+        } else if (obj instanceof Throwable) {
+            // 因为异常的超类Throwable中的cause是引用的自己，有循环引用。目前protostuff还不能处理，fst倒是可以。
+            // 所以，自己简单将其序列化，只返回类名和message。
+            byte[] nameBytes = obj.getClass().getName().getBytes("UTF-8");
+            Throwable throwable = (Throwable) obj;
+            String message = throwable.getMessage();
+            byte[] messageBytes = null;
+            if (message != null) {
+                messageBytes = message.getBytes("UTF-8");
+            }
+            int nameLength = nameBytes.length;
+            int totalLength = nameLength;
+            if (messageBytes != null) {
+                totalLength += messageBytes.length;
+            }
+            check(totalLength + 9);
+            byteBuffer.put((byte) 16);
+            byteBuffer.putInt(totalLength);
+            byteBuffer.putInt(nameLength);
+            byteBuffer.put(nameBytes);
+            if (messageBytes != null) {
+                byteBuffer.put(messageBytes);
+            }
+
+        } else {
+            cls = obj.getClass();
+            Schema schema = RuntimeSchema.getSchema(cls);
+            LinkedBuffer buffer = LinkedBuffer.allocate();
+            @SuppressWarnings("unchecked")
+            byte[] bytes = ProtostuffIOUtil.toByteArray(obj, schema, buffer);
+            int length = bytes.length;
+            byte[] nameBytes = cls.getName().getBytes("UTF-8");
+            int nameLength = nameBytes.length;
+            int totalLength = 9 + nameLength + length;
+            check(totalLength);
+            byteBuffer.put((byte) 0);
+            byteBuffer.putInt(totalLength);
+            byteBuffer.putInt(nameLength);
+            byteBuffer.put(nameBytes);
+            byteBuffer.put(bytes);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private byte[] mapToBytes(Class clazz, Map map) {
+        Schema schema = RuntimeSchema.getSchema(clazz);
+        StringMapSchema collectionSchema = new StringMapSchema(schema);
+        LinkedBuffer buffer = LinkedBuffer.allocate(1024);
+        return ProtostuffIOUtil.toByteArray(map, collectionSchema, buffer);
+    }
+
+    @SuppressWarnings("unchecked")
+    private byte[] collectToBytes(Class clazz, Collection list) {
+        Schema schema = RuntimeSchema.getSchema(clazz);
+        MessageCollectionSchema collectionSchema = new MessageCollectionSchema(schema);
+        LinkedBuffer buffer = LinkedBuffer.allocate(1024);
+        return ProtostuffIOUtil.toByteArray(list, collectionSchema, buffer);
+    }
+
+    /**
+     * 检查buffer中的剩余空间是否能放下新加入的数据，不行就扩容。
+     * 扩容的大小为（size + 256）。
+     *
+     * @param size buffer要新加入的数据大小
+     */
+    private void check(int size) {
+        if (byteBuffer.remaining() < size) {
+            int cap = byteBuffer.capacity() + size + 256;
+            ByteBuffer buffer = ByteBuffer.allocate(cap);
+            byteBuffer.flip();
+            buffer.put(byteBuffer);
+            byteBuffer = buffer;
+        }
     }
 }
